753cf68475980a3baf33ca5c09d9aec796b5c420
LucGulden <luc.gulden@viacesi.fr>
2021-10-11 21:11:18 +0200

    added UI and Images and Freeze

---
+++ Assets/VR-Vs-KMS/Scripts/CameraPlayPauseBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+
+public class CameraPlayPauseBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Toggle>().onValueChanged.AddListener(delegate {
+            FreezeCamera();
+        });
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void FreezeCamera()
+    {
+        if (GetComponent<Toggle>().isOn)
+        {
+            VuforiaRenderer.Instance.Pause(true);
+        }
+        else
+        {
+            VuforiaRenderer.Instance.Pause(false);
+        }
+    }
+}
